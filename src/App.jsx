d81847a27989a462b27ce341a10df5644256819c
Andrea Cseri <andrea.cseri27@gmail.com>
2024-06-27 09:53:57 -0700

    finished react intro exercise

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import HomePage from './components/HomePage'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1>React</h1>
+    <p>paragraph</p>
+    <HomePage />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/HomePage.jsx
@@ -0,0 +1,23 @@
+function HomePage(){
+
+    return (
+        <>
+        <h1>React HomePage</h1>
+        <form id="form">
+        <label for="form">Form</label>
+        <input type="text" id="input" />
+        <button type ="submit">Submit</button>
+        <ul>
+            <li>
+                Mrs Doubtfire
+            </li>
+        </ul>
+
+
+
+        </form>
+        </>
+      )
+}
+
+export default HomePage

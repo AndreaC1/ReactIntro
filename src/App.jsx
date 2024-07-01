9e9d6874b5f8d5ca466950be0b805053eb6a0914
Andrea Cseri <andrea.cseri27@gmail.com>
2024-07-01 07:57:55 -0700

    completed exercise

--- src/App.jsx
+++ src/App.jsx
@@ -8,20 +8,7 @@ function App() {
   return (
     <>
 
-    
-    <nav>
-    <ul>
-      <li>
-        <a href="#"> Home</a>
-      </li>
-      <li>
-        <a href="#"> Movies</a>
-      </li>
-      <li>
-        <a href="#"> About</a>
-      </li>
-    </ul>
-    </nav>
+
     <HomePage />
     </>
   )
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,30 +1,51 @@
 import "./HomePage.css"
+import { useState } from "react";
 
 
 
 function HomePage(){
 
+    const [movies, setMovies] = useState([]);
+    const [newMovie, setNewMovie] = useState("");
+
+    const handleInputChange = (e) => {
+        setNewMovie(e.target.value);
+    }
+    const handleFormSubmit = (e) => {
+        e.preventDefault();
+        setMovies([...movies, newMovie]);
+        console.log("test");
+        setNewMovie('');
+    }
+
+    const handleRemoveMovie = (index) =>{
+        setMovies(movies.filter((_, i)=> i !== index));
+    }
+
     return (
         <>
         <h1>Movies</h1>
-        <form id="form">
-        <label for="form">Enter Movie</label>
-        <input type="text" id="input" />
+        <form onSubmit={handleFormSubmit}>
+        
+        <label htmlFor="movieInput">Enter Movie</label>
+        <input 
+        type="text" 
+        id="movieInput"
+        name="movieInput" 
+         onChange={handleInputChange}
+        value = {newMovie}
+        />
         <button type ="submit">Add movie</button>
         <ul>
-            <li>
-                Mrs Doubtfire
-            </li>
-            <li>
-                Office Space
-            </li>
-            <li>
-                Devil Wears Prada
-            </li>
+         {
+            movies.map((movie,index) =>(
+                <li key={index}>
+                    {movie}
+                    <button onClick={() => handleRemoveMovie(index)}>Remove</button>
+                </li>
+            ))
+         }
         </ul>
-
-
-
         </form>
         </>
       )
